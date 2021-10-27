80b1c554e7dfc88337ffde2f394a6a43463d19c9
ToniGomez <agomezj@cifpfbmoll.eu>
2021-10-27 20:28:51 +0200

    Basic structure for the array exercicies

---
+++ DWEC/array-exercices/src/js/script.js
@@ -0,0 +1,16 @@
+
+function options(param){
+    let valor = document.getElementById("entrada").value
+    param = parseInt(param)
+
+    switch(param){
+
+        case 0:
+            console.log("Numero 0")
+            break
+        case 1:
+            console.log("Numero 1")
+    }
+}
+
+console.log("Hola estoy")
